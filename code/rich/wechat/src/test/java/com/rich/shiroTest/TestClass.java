a78b9c122e9fb82cf70c1583bdb4522da35d9fc2
hyx <730530507@qq.com>
2018-02-24 10:14:03 +0800

    1

---
+++ code/rich/wechat/src/test/java/com/rich/shiroTest/TestClass.java
@@ -0,0 +1,35 @@
+package com.rich.shiroTest;
+
+import junit.framework.Assert;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Test;
+
+public class TestClass {
+    @Test
+    public void testHelloworld() {
+          //1、获取 SecurityManager 工厂，此处使用 Ini 配置文件初始化 SecurityManager
+        Factory<SecurityManager> factory =
+                new IniSecurityManagerFactory("classpath:shiro.ini");
+          //2、得到 SecurityManager 实例 并绑定给 SecurityUtils
+        org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+          //3、得到 Subject 及创建用户名/密码身份验证 Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+        try {
+          //4、登录，即身份验证
+            subject.login(token);
+        } catch (AuthenticationException e) {
+          //5、身份验证失败
+        }
+        Assert.assertEquals(true, subject.isAuthenticated());           //断言用户已经登录
+          //6、退出
+        subject.logout();
+    }
+}
